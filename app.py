ed4437c1d143775b126f896b2897845e2e1b7173
zhanglian <2875165755@qq.com>
2023-05-19 20:49:09 +0800

    /softpower接口调整

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ CORS(app, resources=r'/*')	# 注册CORS, "/*" 允许访问所有api
 dbcon = pymysql.connect(
   host="127.0.0.1",
   user="root",
-  password="123456",
+  password="root",
   db = "china_data",
   port=3306,
   charset='utf8mb4',
@@ -61,9 +61,9 @@ def data_provided_city():
     for v in city_value:
         sql = "select "+v+" from "+table+" where 城市="+city_name
         #Mysql8.x
-        sql_rank="WITH a AS(SELECT 城市,RANK( ) OVER (ORDER BY "+v+" DESC) city_rank FROM "+table+") SELECT city_rank FROM a WHERE 城市="+city_name
+        # sql_rank="WITH a AS(SELECT 城市,RANK( ) OVER (ORDER BY "+v+" DESC) city_rank FROM "+table+") SELECT city_rank FROM a WHERE 城市="+city_name
         #Mysql5.x
-        # sql_rank="SELECT aaa.rank from(select `城市`,`"+v+"`, @rk := @rk+1 as rank from "+table+",(select @rk:=0)  a order by `"+v+"` desc ) as aaa where `城市` ="+city_name
+        sql_rank="SELECT aaa.rank from(select `城市`,`"+v+"`, @rk := @rk+1 as rank from "+table+",(select @rk:=0)  a order by `"+v+"` desc ) as aaa where `城市` ="+city_name
         lock.acquire()
         data = pd.read_sql(sql,dbcon)
         data_rank=pd.read_sql(sql_rank,dbcon)
@@ -77,30 +77,42 @@ def data_provided_city():
 @app.route("/softpower")
 def data_provided_index():
     data_index = request.args.get("data_index")
-    year= request.args.get("year")
+    # year= request.args.get("year")
+    result=[]
     res=[]
+    res2=[]
     all_index=['生态禀赋','文化资源','政策地位','经济规模','交通规模','创新能力','基本保障','生活水平','主流评价','教育服务','医疗服务','文化服务','主流媒体','网络接入','舆情干预','媒体影响','群体情绪','城市标签','就学吸引','就业吸引','旅游吸引','外资吸引','会展竞争']
     all_city=['石家庄','太原','呼和浩特','沈阳','长春','哈尔滨','南京','杭州','合肥','福州','南昌','郑州','武汉','长沙','广州','南宁','海口','成都','贵阳','昆明','拉萨','西安','兰州','西宁','银川','乌鲁木齐','深圳','大连','宁波','青岛','厦门','苏州']
-    if year not in ['2020','2021']:
-        return jsonify(res)
+    # if year not in ['2020','2021']:
+    #     return jsonify(res)
     if data_index not in all_index:
         return jsonify(res)
-    table="china_data_"+year
+    table="china_data_2020"
+    table2="china_data_2021"
     for city_name in all_city:
         city_name="\""+city_name+"\""
         sql = "select "+data_index+" from "+table+" where 城市="+city_name
+        sql2 = "select "+data_index+" from "+table2+" where 城市="+city_name
         #Mysql8.x
-        sql_rank="WITH a AS(SELECT 城市,RANK( ) OVER (ORDER BY "+data_index+" DESC) city_rank FROM "+table+") SELECT city_rank FROM a WHERE 城市="+city_name
+        # sql_rank="WITH a AS(SELECT 城市,RANK( ) OVER (ORDER BY "+data_index+" DESC) city_rank FROM "+table+") SELECT city_rank FROM a WHERE 城市="+city_name
         #Mysql5.x
-        # sql_rank="select aaa.rank from(select `城市`,`"+data_index+"`, @rk := @rk+1 as rank from "+table+",(select @rk:=0)  a order by `"+data_index+"` desc ) as aaa where `城市` ="+city_name
+        sql_rank="select aaa.rank from(select `城市`,`"+data_index+"`, @rk := @rk+1 as rank from "+table+",(select @rk:=0)  a order by `"+data_index+"` desc ) as aaa where `城市` ="+city_name
+        sql_rank2="select aaa.rank from(select `城市`,`"+data_index+"`, @rk := @rk+1 as rank from "+table2+",(select @rk:=0)  a order by `"+data_index+"` desc ) as aaa where `城市` ="+city_name
         lock.acquire()
         data = pd.read_sql(sql,dbcon)
+        data2 = pd.read_sql(sql2,dbcon)
         data_rank=pd.read_sql(sql_rank,dbcon)
+        data_rank2=pd.read_sql(sql_rank2,dbcon)
         lock.release()
         temp_data={'name':city_name[1:-1],'data':float(data.iloc[0, 0]),'rank':float(data_rank.iloc[0, 0])}
+        temp_data2={'name':city_name[1:-1],'data':float(data2.iloc[0, 0]),'rank':float(data_rank2.iloc[0, 0])}
         res.append(temp_data)
+        res2.append(temp_data2)
+
     # print(res)
-    return jsonify(res)
+    result.append({"year": 2020, "data": res})
+    result.append({"year": 2021, "data": res2})
+    return jsonify(result)
 
 @app.route("/scatter")
 def data_provided_scatter():
