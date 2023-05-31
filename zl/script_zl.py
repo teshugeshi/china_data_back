b70f82270800a34e23106a4b2fc18c2fcfc0194d
zhanglian <2875165755@qq.com>
2023-05-31 13:12:07 +0800

    脚本开发完成-张炼

---
+++ zl/script_zl.py
@@ -0,0 +1,24 @@
+from cmw.script import *
+
+if __name__ == '__main__':
+    get_standard_value('economy_2020','GDP总量','标准化值')
+    get_standard_value('economy_2021','GDP总量','标准化值')
+
+    get_standard_value('traffic_2020','客运总量','标准化值')
+    get_standard_value('traffic_2021','客运总量','标准化值')
+
+    get_standard_value('innovate_2020','创新指数','标准化值')
+    get_standard_value('innovate_2021','创新指数','标准化值')
+
+    get_standard_value('basic_security_2020','平均值','标准化值')
+    get_standard_value('basic_security_2021','平均值','标准化值')
+
+    get_average_value('living_standard_2020','标准化值','标准化值1','得分')
+    get_average_value('living_standard_2021','标准化值','标准化值1','得分')
+
+    get_standard_value('main_evaluate_2020','文明城市得分','标准化值')
+    get_standard_value('main_evaluate_2021','文明城市得分','标准化值')
+
+    get_average_value('education_service_2020','标准化值','标准化值1','平均值')
+    get_average_value('education_service_2021','标准化值','标准化值1','平均值')
+
